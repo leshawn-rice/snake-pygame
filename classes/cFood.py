37c32e69999849d87c026557e19c9f4b58447b3d
Leshawn Rice <63131837+leshawn-rice1@users.noreply.github.com>
2020-04-03 21:53:19 -0700

    Update cFood.py

--- classes/cFood.py
+++ classes/cFood.py
@@ -1,13 +1,24 @@
 from setup.definitions import *
 from setup.settings import *
 
+
 class Food(object):
-	def __init__(self):
-		self.size = 10
-		self.xPos = random.randint(borders[0],borders[1])
-		self.yPos = random.randint(borders[0],borders[1])
-		self.pos = (self.xPos,self.yPos)
-		self.img = pygame.image.load('images/food.png')
-		self.img = pygame.transform.scale(self.img,(self.size,self.size))
-	def displaySelf(self):
-		gameDisplay.blit(self.img,self.pos)
+    def __init__(self):
+        '''
+        Called anytime a food object is initialized.
+        Puts the food at a random x-y coordinate and
+        sets the image and size for the food.
+        '''
+        self.size = 10
+        self.xPos = random.randint(borders[0], borders[1])
+        self.yPos = random.randint(borders[0], borders[1])
+        self.pos = (self.xPos, self.yPos)
+        self.img = pygame.image.load('images/food.png')
+        self.img = pygame.transform.scale(self.img, (self.size, self.size))
+
+    def displaySelf(self):
+        '''
+        Displays the current food object to the
+        screen based on the food's current position
+        '''
+        gameDisplay.blit(self.img, self.pos)
